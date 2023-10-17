882df44fbcb103adf1a967bc08e1ca4a9f12e18f
Guilherme Luz <luzguilherme.dev@gmail.com>
2023-10-16 22:24:15 -0300

    style(accordion): tuning the accordion
    Adjusting actions with the accordion and adjusting responsiveness

--- scripts/accordion.js
+++ scripts/accordion.js
@@ -1,14 +1,33 @@
-const question = document.querySelectorAll("#faq li");
+const questions = document.querySelectorAll("#faq li"); 
 
-
-question.forEach(e => {
+questions.forEach(e => {
     e.addEventListener('click', () => {
         const response = e.querySelector('.faq_response')
         if (response.classList.contains('active')) {
-            e.querySelector('.faq_response').classList.remove('active');
-            console.log(response);
+           removeActive(e, {all: false});
+           
+        
         } else {
+            removeActive(e, {all : true})
             response.classList.add("active");
+            e.firstElementChild.querySelector('p').style.opacity = '1';
+            e.querySelector('i').style.transform = 'rotate(180deg)';
         }
     })
 })
+
+const removeActive= (e, all)=>{
+    if(all.all){
+        questions.forEach(e=>{
+            e.querySelector('.faq_response').classList.remove('active');
+            e.firstElementChild.querySelector('p').style.opacity = '.7';
+            e.querySelector('i').style.transform = 'rotate(0)';
+        })
+    }else {
+        e.querySelector('.faq_response').classList.remove('active');
+        e.firstElementChild.querySelector('p').style.opacity = '.7';
+        e.querySelector('i').style.transform = 'rotate(0)';
+
+    }
+}
+   
